0a949d54b08871ffa08ffe49b5908916bd619304
Chuhnakov <32620844+Chuhnakov@users.noreply.github.com>
2020-12-07 23:56:38 +0300

    Chuhn
    Lesson 3

---
+++ Lesson3/src/main/java/ru/chuhnakov/Main.java
@@ -0,0 +1,17 @@
+package ru.chuhnakov;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] temp = s.beautifulArray(5);
+
+        for(int i = 0; i < temp.length; ++i) {
+            System.out.print(temp[i] + " ");
+        }
+        System.out.println("");
+        System.out.println("___________Patches________________");
+
+        System.out.print(s.minPatches(temp,20));
+    }
+}

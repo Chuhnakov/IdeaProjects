1f36eb2420962fd841f34a9b92c658de4f704b32
Chuhnakov <32620844+Chuhnakov@users.noreply.github.com>
2020-12-08 22:13:11 +0300

    ref Code

--- Lesson2/src/test/java/Tester.java
+++ Lesson2/src/test/java/Tester.java
@@ -6,10 +6,10 @@ import static junit.framework.Assert.*;
 public class Tester {
 
     @Test
-    public void AreaTest(){
+    public void AreaTest() {
         Solution s = new Solution();
-        assertEquals(s.maxArea(new int[]{3, 5, 2, 4,10,9,6}),25);
-        assertEquals(s.maxArea(new int[]{3, 9, 3, 4,7,2,12,6}),45);
-        assertEquals(s.maxArea(new int[]{1, 8, 6, 2, 5, 4, 8, 3, 7}),49);
+        assertEquals(s.maxArea(new int[]{3, 5, 2, 4, 10, 9, 6}), 25);
+        assertEquals(s.maxArea(new int[]{3, 9, 3, 4, 7, 2, 12, 6}), 45);
+        assertEquals(s.maxArea(new int[]{1, 8, 6, 2, 5, 4, 8, 3, 7}), 49);
     }
 }
